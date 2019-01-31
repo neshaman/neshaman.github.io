1cbd1571158edbda49226c66908165bc27346c12
Nenad Manoilov <nmanoilov@alamo.edu>
2019-01-30 23:03:46 -0600

    deploy

---
+++ precache-manifest.0e4f6af5dc9f22abc18745c48047f441.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "d82b1d8aee563cde10508e3ba34aff45",
+    "url": "/img/the-table.d82b1d8a.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "db099ffb32a34aad27bc8159fca2ac59",
+    "url": "/img/elasticsearch.db099ffb.png"
+  },
+  {
+    "revision": "edf29a691a0c26a71a89",
+    "url": "/js/app.929c77b8.js"
+  },
+  {
+    "revision": "11d6b35e023aea84f6fa",
+    "url": "/js/chunk-vendors.75688ad1.js"
+  },
+  {
+    "revision": "f4de9d3b75053f2533a015b13fb5e210",
+    "url": "/img/gcp.f4de9d3b.png"
+  },
+  {
+    "revision": "56b054e1e091d82f1403",
+    "url": "/js/about.256f241c.js"
+  },
+  {
+    "revision": "9f7fe2dffa14572cbd839ffa69f13708",
+    "url": "/img/the-bike.9f7fe2df.jpg"
+  },
+  {
+    "revision": "9a4ef77bcf75ab441251623b9ff45709",
+    "url": "/img/wordpress.9a4ef77b.png"
+  },
+  {
+    "revision": "54c3dccace252b7fc898ab9d51a3616c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "057a0d6b4675facaf2b04a856b3c3ce7",
+    "url": "/img/avid.057a0d6b.png"
+  },
+  {
+    "revision": "13ff09496b2d53ebf33a568ff243b366",
+    "url": "/img/laravel.13ff0949.png"
+  },
+  {
+    "revision": "54f2d07add90aa84184d0f9e2b610a4a",
+    "url": "/img/php.54f2d07a.png"
+  },
+  {
+    "revision": "8ba982fb67c7acf8546dcd412c4d9906",
+    "url": "/img/road.8ba982fb.jpeg"
+  },
+  {
+    "revision": "97add6927fc586f67ea4c3cbe02f6f9c",
+    "url": "/img/node.97add692.png"
+  },
+  {
+    "revision": "5dd0f34f014eaed4e3820f3c1b2ef7be",
+    "url": "/img/me-small.5dd0f34f.jpg"
+  },
+  {
+    "revision": "0a00d33347631f9f8ae58e5e95dfa45d",
+    "url": "/img/javascript.0a00d333.png"
+  },
+  {
+    "revision": "1caad2dbf7fb4de62f22af8c7d99ccca",
+    "url": "/img/html5.1caad2db.png"
+  },
+  {
+    "revision": "3f458402a67d6e6eb548a576b25cc284",
+    "url": "/img/hero-img.3f458402.jpg"
+  },
+  {
+    "revision": "8ae1c0c7e8d67ef59eade9f14d40b47d",
+    "url": "/img/css3.8ae1c0c7.png"
+  },
+  {
+    "revision": "810e33b20856802f8b0e434fc530b7e2",
+    "url": "/img/3dsmax.810e33b2.png"
+  },
+  {
+    "revision": "11d6b35e023aea84f6fa",
+    "url": "/css/chunk-vendors.f9c029a2.css"
+  },
+  {
+    "revision": "edf29a691a0c26a71a89",
+    "url": "/css/app.7858824d.css"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.0e4f6af5dc9f22abc18745c48047f441.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "section6"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
